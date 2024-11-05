44d7acbaf6eeccd67d8c02b602c3cc099ac3fed4
Justus Geramb <admin@justix.dev>
2024-11-05 01:27:30 +0100

    fix: eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
-export default {
-  root: true,
+// @ts-check
+
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
   rules: {
     "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 1 }],
-    "arrow-parens": "on",
-    "no-console": "on",
+    "arrow-parens": ["error", "as-needed"],
+    "no-console": "warn",
     indent: ["error", 2],
     "comma-dangle": ["error", "never"],
     "max-len": ["warn", 100, { ignoreRegExpLiterals: true }],
     "linebreak-style": ["error", "unix"],
     "import/no-cycle": "off"
   }
-};
+});
