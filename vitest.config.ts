4833130b1e99badcd83948f2b0f68bda20bcd9fa
Justus Geramb <admin@justix.dev>
2024-11-07 15:01:20 +0100

    feat: add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  test: {
+    alias: {
+      "#types": resolve(__dirname, "./types"),
+      "#utils": resolve(__dirname, "./src/utils"),
+      "#clients": resolve(__dirname, "./src/clients"),
+      "#payloads": resolve(__dirname, "./src/payloads")
+    }
+  }
+});
