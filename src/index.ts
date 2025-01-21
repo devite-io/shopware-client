93106003278d7ad1edde69e6f05a401374ceacde
Justus Geramb <admin@justix.dev>
2025-01-21 12:46:01 +0100

    fix: export payloads for typed direct api usage via doRequest

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,7 @@ export { default as ShopwareClient } from "#clients/ShopwareClient";
 export { default as AdminShopwareClient } from "#clients/AdminShopwareClient";
 export { default as StoreShopwareClient } from "#clients/StoreShopwareClient";
 
+export { default as HTTPRequestMethod } from "#http/HTTPRequestMethod";
 export { default as ShopwareError } from "#http/ShopwareError";
+
+export * from "./payloads";
---
+++ src/payloads/index.ts
@@ -0,0 +1,4 @@
+export { default as BinaryPayload } from "./BinaryPayload";
+export { default as HtmlPayload } from "./HtmlPayload";
+export { default as JsonPayload } from "./JsonPayload";
+export { default as Payload } from "./Payload";
