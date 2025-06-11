4260334b55b9b331e94bb238ad6774e38c8a0208
Justus Geramb <admin@justix.dev>
2025-06-11 15:59:33 +0200

    feat: add guest download route

--- src/clients/store/DocumentClient.ts
+++ src/clients/store/DocumentClient.ts
@@ -1,4 +1,5 @@
 import BinaryPayload from "#payloads/BinaryPayload";
+import JsonPayload from "#payloads/JsonPayload";
 import Client from "../Client";
 import type StoreShopwareClient from "../StoreShopwareClient";
 import ShopwareError from "#http/ShopwareError";
@@ -9,7 +10,7 @@ class DocumentClient extends Client {
    * @throws {ShopwareError | import('ofetch').FetchError} if the request failed
    */
   async download(id: string, deepLinkCode: string): Promise<DocumentDownloadResponse> {
-    const response = await this.post(
+    const response = await this.get(
       `/document/download/${id}/${deepLinkCode}`,
       (this.client as StoreShopwareClient).withContextToken()
     );
@@ -18,6 +19,31 @@ class DocumentClient extends Client {
       return (response.body as BinaryPayload).data as DocumentDownloadResponse;
 
     throw new ShopwareError("Failed to download document", response);
+  }
+
+  /**
+   * @throws {ShopwareError | import('ofetch').FetchError} if the request failed
+   */
+  async downloadAsGuest(
+    id: string,
+    deepLinkCode: string,
+    email: string,
+    billingAddressZipCode: string
+  ): Promise<DocumentDownloadResponse> {
+    const response = await this.post(
+      `/document/download/${id}/${deepLinkCode}`,
+      (this.client as StoreShopwareClient).withContextToken({
+        body: new JsonPayload({
+          email,
+          zipcode: billingAddressZipCode
+        })
+      })
+    );
+
+    if (response.statusCode === 200)
+      return (response.body as BinaryPayload).data as DocumentDownloadResponse;
+
+    throw new ShopwareError("Failed to download document as guest", response);
   }
 }
 
